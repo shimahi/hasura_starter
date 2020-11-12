df926469c23a8885fc366e82751c46a447c8f728
shimahi <shimahi@paroleise.com>
2020-11-12 20:31:38 +0900

    emotionv11に対応

--- init.sh
+++ init.sh
@@ -90,15 +90,17 @@ touch README.md
 echo "# $1" >>README.md
 
 ### import npm packages
-yarn workspace client add react react-dom @emotion/core @emotion/styled ress twin.macro @apollo/client graphql
+yarn workspace client add react react-dom @emotion/react @emotion/styled ress twin.macro @apollo/client graphql
 yarn workspace client add -D typescript @types/{node,react,react-dom} \
-  webpack webpack-{cli,dev-server} {ts,style,css,url,file,babel}-loader html-webpack-plugin worker-plugin dotenv-webpack \
+  webpack webpack-{cli,dev-server} {ts,style,css,babel}-loader html-webpack-plugin worker-plugin dotenv-webpack \
   @emotion/babel-preset-css-prop \
-  @babel/{core,preset-env,preset-react} \
+  @babel/{core,preset-env,preset-react,plugin-transform-runtime} \
   @graphql-codegen/{cli,typescript,typescript-operations,typescript-react-apollo} \
   prettier eslint eslint-config-{airbnb-typescript,prettier} eslint-plugin-{import,jsx-a11y,prettier,react,react-hooks} \
   @typescript-eslint/eslint-plugin @typescript-eslint/parser \
   lint-staged husky &
+
+
 
 wait
 rm -rf node_modules &
